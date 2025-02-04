9a48a64092880f6f658dbd265415fe7266af38fe
Jelcoo <53396500+Jelcoo@users.noreply.github.com>
2025-02-04 19:23:59 +0000

    Auto fetch user on app load

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,6 +1,13 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
+import { onMounted } from 'vue';
+import { useUserStore } from '@/stores/user';
 import NavbarComponent from '@/components/NavbarComponent.vue';
+
+onMounted(() => {
+    const userStore = useUserStore();
+    userStore.autoLogin();
+});
 </script>
 
 <template>
--- vue/src/stores/user.ts
+++ vue/src/stores/user.ts
@@ -13,6 +13,7 @@ export const useUserStore = defineStore('user', {
     }),
     getters: {
         fullName: (state) => `${state.first_name} ${state.last_name}`,
+        isAuthenticated: (state) => !!state.token,
     },
     actions: {
         login(email: string, password: string) {
@@ -30,6 +31,33 @@ export const useUserStore = defineStore('user', {
                         this.email = res.data.user.email;
                         this.created_at = res.data.user.created_at;
                         this.token = res.data.token;
+
+                        localStorage.setItem('token', res.data.token);
+
+                        resolve(this);
+                    })
+                    .catch((error) => reject(error));
+            });
+        },
+        autoLogin() {
+            const token = localStorage.getItem('token');
+            if (!token) {
+                return;
+            }
+
+            axios.defaults.headers.common['Authorization'] = 'Bearer ' + token;
+
+            return new Promise((resolve, reject) => {
+                axios
+                    .get('/me')
+                    .then((res) => {
+                        this.id = res.data.id;
+                        this.username = res.data.username;
+                        this.first_name = res.data.first_name;
+                        this.last_name = res.data.last_name;
+                        this.email = res.data.email;
+                        this.created_at = res.data.created_at;
+
                         resolve(this);
                     })
                     .catch((error) => reject(error));
