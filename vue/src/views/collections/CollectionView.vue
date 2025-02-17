7eeb967d2240a7236d7a740c79bd8e791c6bd5f6
Jelcoo <53396500+Jelcoo@users.noreply.github.com>
2025-02-17 15:03:44 +0000

    Show 404 if collection does not exist

--- vue/src/views/collections/CollectionView.vue
+++ vue/src/views/collections/CollectionView.vue
@@ -1,6 +1,8 @@
 <template>
-    <main>Welcome to collection</main>
     <div v-if="collectionStore.currentCollection.loading">Loading...</div>
+    <div v-else-if="notFound">
+        <NotFoundView />
+    </div>
     <div v-else>
         <h1>{{ collectionStore.currentCollection }}</h1>
     </div>
@@ -8,14 +10,20 @@
 
 <script setup lang="ts">
 import { useCollectionStore } from '@/stores/collection';
-import { onBeforeMount } from 'vue';
+import { onBeforeMount, ref } from 'vue';
 import { useRoute } from 'vue-router';
+import NotFoundView from '@/views/NotFoundView.vue';
 
 const collectionStore = useCollectionStore();
-const router = useRoute();
+const route = useRoute();
+const notFound = ref(false);
 
 onBeforeMount(() => {
-    const collectionId = router.params.id;
-    collectionStore.getCollection(collectionId, ['stamps', 'author']);
+    const collectionId = Number(route.params.id);
+    collectionStore.getCollection(collectionId, ['stamps', 'author']).catch((error) => {
+        if (error.status === 404) {
+            notFound.value = true;
+        }
+    });
 });
 </script>
