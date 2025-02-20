fe631c24a3ac606da175733effb1ec379da9c32c
Jelcoo <53396500+Jelcoo@users.noreply.github.com>
2025-02-20 17:24:01 +0000

    Display status errors on collection edit

--- vue/src/views/collections/CollectionEditView.vue
+++ vue/src/views/collections/CollectionEditView.vue
@@ -1,5 +1,7 @@
 <template>
-    <ContainerComponent :loading="!collection">
+    <ForbiddenView v-if="status === 403" />
+    <NotFoundView v-else-if="status === 404" />
+    <ContainerComponent v-else :loading="loading || !collection">
         <CollectionCreateView :collection="collection" />
     </ContainerComponent>
 </template>
@@ -11,15 +13,26 @@ import { useRoute } from 'vue-router';
 import CollectionCreateView from './CollectionCreateView.vue';
 import type { Collection } from '@/stores/types';
 import ContainerComponent from '@/components/ContainerComponent.vue';
+import ForbiddenView from '../status/ForbiddenView.vue';
+import NotFoundView from '../status/NotFoundView.vue';
 
 const collectionStore = useCollectionStore();
 const route = useRoute();
+
+const loading = ref(true);
 const collection = ref<Collection>();
+const status = ref(0);
 
 onBeforeMount(() => {
     const collectionId = Number(route.params.id);
-    collectionStore.getCollection(collectionId).then((res) => {
-        collection.value = res.data;
-    });
+    collectionStore
+        .getCollection(collectionId)
+        .then((res) => {
+            collection.value = res.data;
+            loading.value = false;
+        })
+        .catch((error) => {
+            status.value = error.status;
+        });
 });
 </script>
