591739cfb6c811eb9b621a597b0f56617f2bda41
Jelcoo <53396500+Jelcoo@users.noreply.github.com>
2025-02-22 15:20:22 +0000

    Improve entrypoint

--- php/entrypoint.sh
+++ php/entrypoint.sh
@@ -1,5 +1,17 @@
 #!/bin/ash
 
-composer install --no-interaction --no-progress --optimize-autoloader
+if [ -f /app/vendor/autoload.php ]; then
+    echo "vendor/autoload.php already exists"
+else
+    echo "Installing composer dependencies"
+    composer install --no-interaction --no-progress --optimize-autoloader
+fi
+
+if [ -f /app/config.php ]; then
+    echo "config.php already exists"
+else
+    echo "Creating config.php"
+    cp config.php.example config.php
+fi
 
 php-fpm
