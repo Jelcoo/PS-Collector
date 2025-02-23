7d2d513ad1db7de74d8ef5fa5bd58a00e69d2635
Jelcoo <53396500+Jelcoo@users.noreply.github.com>
2025-02-23 12:58:05 +0000

    Check proper filepath in entrypoint

--- php/entrypoint.sh
+++ php/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/ash
 
-if [ -f /app/vendor/autoload.php ]; then
+if [ -f /app/backend/vendor/autoload.php ]; then
     echo "vendor/autoload.php already exists"
 else
     echo "Installing composer dependencies"
     composer install --no-interaction --no-progress --optimize-autoloader
 fi
 
-if [ -f /app/config.php ]; then
+if [ -f /app/backend/config.php ]; then
     echo "config.php already exists"
 else
     echo "Creating config.php"
