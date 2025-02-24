abddad6c24e98b379b88f4a95af35ccd9d869137
Jelcoo <53396500+Jelcoo@users.noreply.github.com>
2025-02-24 11:18:55 +0100

    Copy env if not existing

--- vue/entrypoint.sh
+++ vue/entrypoint.sh
@@ -1,5 +1,17 @@
 #!/bin/ash
 
-pnpm install --frozen-lockfile
+if [ -d /app/frontend/node_modules ]; then
+    echo "node_modules already exists"
+else
+    echo "Installing pnpm dependencies"
+    pnpm install --frozen-lockfile
+fi
+
+if [ -f /app/frontend/.env ]; then
+    echo ".env already exists"
+else
+    echo "Creating .env"
+    cp .env.example .env
+fi
 
 pnpm run dev
